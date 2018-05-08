1edcd3826e00da7fbae46669fa0b71089b5f606d
Ryan Shores <ryan.shores@me.com>
2018-05-08 17:28:14 +0000

    hurricane add/edit/delete card add/edit delete

--- routes/storms.js
+++ routes/storms.js
@@ -57,12 +57,24 @@ router.post("/edit/:id", function(req, res) {
     });
 });
 // delete
-router.get('/delete', function(req, res){
-    res.send('Delete');
+router.get('/:id/delete', function(req, res){
+    Storms.findByIdAndRemove(req.params.id, function(err){
+        if(err) { res.send(err); }
+        else { res.redirect("/storms")}
+    });
 });
-
-// CARDS
-// get
+// change storm active
+router.post("/:id/active", function(req, res){
+   Storms.findById(req.params.id, function(err, foundStorm) {
+       if( err ){ res.send(err); }
+       else{
+           foundStorm.isActive = req.body.isActive;
+           foundStorm.save();
+           res.redirect("/storms/" + foundStorm._id);
+       }
+   });
+});
+// get individual storm
 router.get('/:id', function(req, res){
     let cards = [];
     Storms.findById(req.params.id).populate("cards").exec(function( err, foundStorm ){
@@ -70,7 +82,10 @@ router.get('/:id', function(req, res){
         else { res.render("./storms/storm", { storm: foundStorm, cards: cards }); }
     });
 });
+
+// CARDS
 // add
+// id here is the parent storms id
 router.get('/:id/add/card', function(req, res){
     res.render("./storms/addcard", { stormid: req.params.id });
 });
@@ -107,8 +122,42 @@ router.post('/:id/add/card', function(req, res){
         }
     });
 });
-
-
+// active
+router.post("/:stormid/:cardid/active", function(req, res){
+   Cards.findById(req.params.cardid, function(err, card) {
+       if( err ){ res.send( err ); }
+       else {
+           card.isActive = req.body.isActive;
+           card.save();
+           res.redirect("/storms/" + req.params.stormid);
+       }
+   });
+});
+// edit
+router.get("/:stormid/:cardid/edit", function(req, res) {
+    Cards.findById(req.params.cardid, function(err, card) {
+        if( err ){ res.send( err ); }
+        else { res.render("./storms/cardedit", {card: card, stormid: req.params.stormid} ); }
+    });
+});
+router.post("/:stormid/:cardid/edit", function(req, res) {
+   var card = {
+       title: req.body.title,
+       url: req.body.url,
+       desc: req.body.desc
+   };
+   Cards.findByIdAndUpdate(req.params.cardid, card, function( err, card ){
+       if( err ) { res.send( err ); }
+       else { res.redirect("/storms/" + req.params.stormid); }
+   });
+});
+// delete
+router.get("/:stormid/:cardid/delete", function(req, res) {
+   Cards.findByIdAndRemove(req.params.cardid, function( err ){
+       if( err ) { res.send( err ); }
+       else { res.redirect("/storms/" + req.params.stormid); }
+   });
+});
 
 
 module.exports = router;
